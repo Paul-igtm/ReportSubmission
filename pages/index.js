837f842d50bf19c23f682d7dc89b581a8920832d
OlacodesLA <zaccheausjide@gmail.com>
2022-12-20 22:57:15 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -262,7 +262,7 @@ export default function Home() {
                 </label>
                 <input
                   type="text"
-                  name="MartricNumber"
+                  name="MatricNumber"
                   placeholder="e.g 170211038"
                   className="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-indigo-500 focus:ring-1 rounded-md"
                   required
